ac7c3f6df3538c53704fdda922fe6e7f2cd66bf7
Sergio <sergioasbunf@hotmail.com>
2021-11-07 20:51:22 -0400

    Agregado modelo de usuario y vacunas

---
+++ APIVacunas-Restaurante/Models/Usuario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIVacunas_Restaurante.Models
+{
+    public class Usuario
+    {
+        [Key]
+        public string Carnet { get; set; }
+        [Required]
+        public string NombreCompleto { get; set; } 
+        [Required]
+        public ICollection<Vacuna> Vacunas { get; set; }
+    }
+}
---
+++ APIVacunas-Restaurante/Models/Vacuna.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace APIVacunas_Restaurante.Models
+{
+    public class Vacuna
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Tipo { get; set; }
+        public string Dosis { get; set; }
+        public DateTime FechaDeVacunacion { get; set; }
+        public Usuario usuario { get; set; }
+
+    }
+}
