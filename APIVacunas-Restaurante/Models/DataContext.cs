f07ac4b1efe9593e777a7280f3cefdf77ab9fec2
Sergio <sergioasbunf@hotmail.com>
2021-11-07 20:56:34 -0400

    Agregado DataContext

---
+++ APIVacunas-Restaurante/Models/DataContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace APIVacunas_Restaurante
+{
+    class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
--- APIVacunas-Restaurante/Startup.cs
+++ APIVacunas-Restaurante/Startup.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,7 +27,7 @@ namespace APIVacunas_Restaurante
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddDbContext<DataContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DBAzure")));
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
